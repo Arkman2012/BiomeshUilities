93303a9551b9034764757a3f838db550d2ed0436
Rob Elser <r.elser@icloud.com>
2015-12-10 06:54:49 +0000

    Initial Commit

---
+++ BiomeshUilities/BiomeshUilities.h
@@ -0,0 +1,19 @@
+//
+//  BiomeshUilities.h
+//  BiomeshUilities
+//
+//  Created by Rob Elser on 10/12/2015.
+//  Copyright © 2015 Biomesh Studios, Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BiomeshUilities.
+FOUNDATION_EXPORT double BiomeshUilitiesVersionNumber;
+
+//! Project version string for BiomeshUilities.
+FOUNDATION_EXPORT const unsigned char BiomeshUilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BiomeshUilities/PublicHeader.h>
+
+
